823980a4bb33a7c4d81431f02ea04e532c0e63bc
mochizuki875 <keita@devmo01.local>
2020-03-10 00:03:03 +0900

    chmod in Dockerfile & disable bootsnap in boot.rb

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,10 @@ ENV APP_HOME /app
 RUN mkdir -p $APP_HOME
 WORKDIR $APP_HOME
 ADD . $APP_HOME
-RUN bundle install
+RUN chmod -R 777 ./ && \
+    rm -f tmp/cache/bootsnap-load-path-cache && \
+    rm -rf tmp/cache/bootsnap-compile-cache/  && \
+    bundle install
 
 EXPOSE  3000
 CMD ["rails", "server", "-b", "0.0.0.0"]
--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+# OpenShift上での起動時にエラーが発生するためコメントアウト
+# require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
