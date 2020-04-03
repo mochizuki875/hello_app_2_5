df8ae8441c9b7b89c5f76b7476bc3f5f885a7b56
mochizuki875 <mzk875@gmail.com>
2020-04-03 16:13:28 +0900

    add k8s/manifest.yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
+    render html: 'hello, world(root)!!'
+  end
+
+  def hello_route
     render html: 'hello, world!!'
   end
+
 end
